06c8ca324dcfb1f7e2c48d3efd5fa99ab28f4f78
hello-qu <qujun_1992@163.com>
2021-12-17 21:20:51 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-learn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-learn)
---
+++ index.js
@@ -0,0 +1,7 @@
+// run `node index.js` in the terminal
+
+console.log(`Hello Node.js v${process.versions.node}!`);
+console.log(setImmediate);
+console.log(__filename);
+
+console.log(process);
